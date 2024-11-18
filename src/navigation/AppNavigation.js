75583e8d8abe05d75ff8fbfb2e50119bcf77eadf
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-11-18 16:27:31 -0300

    Falta personalizar Bottom Tab Navigator

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,11 +1,44 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { screen } from "../utils";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// Pantallas
+import { RestaurantsStack } from "./RestaurantsStack";
+import { RankingStack } from "./RankingStack";
+import { FavoritesStack } from "./FavoritesStack";
+import { SearchStack } from "./SearchStack";
+import { AccountStack } from "./AccountStack";
+
+const Tab = createBottomTabNavigator();
 
 export function AppNavigation() {
   return (
-    <View>
-      <Text>AppNavigation</Text>
-    </View>
+    <Tab.Navigator initialRouteName={screen.restaurants.tab}>
+      <Tab.Screen
+        name={screen.restaurants.tab}
+        component={RestaurantsStack}
+        options={{ title: "Restaurantes" }}
+      />
+      <Tab.Screen
+        name={screen.ranking.tab}
+        component={RankingStack}
+        options={{ title: "Ranking" }}
+      />
+      <Tab.Screen
+        name={screen.favorites.tab}
+        component={FavoritesStack}
+        options={{ title: "Favoritos" }}
+      />
+      <Tab.Screen
+        name={screen.search.tab}
+        component={SearchStack}
+        options={{ title: "Buscar" }}
+      />
+      <Tab.Screen
+        name={screen.account.tab}
+        component={AccountStack}
+        options={{ title: "Cuenta" }}
+      />
+    </Tab.Navigator>
   );
 }
