1226bee22452900a3ad23c028bca7fc3f086064c
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-11-20 20:34:43 -0300

    Instalando firebase

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AppNavigation } from "./src/navigation/AppNavigation";
 
+// No hay que usarlo, solo importarlo
+import { initFirebase } from "./src/utils/firebase";
+
 export default function App() {
   return (
     <NavigationContainer>
